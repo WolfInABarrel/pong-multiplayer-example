23a5df182a9a350530e5bf8e236219047ff4c1c6
Jarle Syvertsen <wolfinabarrel@protonmail.com>
2022-03-25 19:19:52 +0100

    Added room logic to sockets

--- sockets.js
+++ sockets.js
@@ -1,28 +1,35 @@
 let readyPlayerCount = 0;
 
+
+
 function listen(io) {
 const pongNameSpace = io.of('/pong');
 pongNameSpace.on('connection', (socket) => {
     console.log('A user connected with id', socket.id);
+    let room;
 
     socket.on('ready', () => {
-        console.log('Player ready with id', socket.id);
+         room = 'room' + Math.floor(readyPlayerCount / 2);
+        socket.join(room);
+
+        console.log('Player ready with id', socket.id, room);
         
         readyPlayerCount++;
 
         if (readyPlayerCount % 2 === 0) {
-            pongNameSpace.emit('startgame', socket.id);    
+            pongNameSpace.in(room).emit('startgame', socket.id);    
         } 
     });
     socket.on('paddleMove', (paddleData) => {
-        socket.broadcast.emit('paddleMove', paddleData);
+        socket.to(room).emit('paddleMove', paddleData);
     });
     socket.on('ballMove', (ballData) => {
-        socket.broadcast.emit('ballMove', ballData);
+        socket.to(room).emit('ballMove', ballData);
     });
 
     socket.on('disconnect', (reason) => {
         console.log(`Client ${socket.id} disconnected: ${reason}`);
+        socket.leave(room);
     });
 });
 
